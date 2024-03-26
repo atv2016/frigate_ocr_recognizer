d8d6c2bdf320e1611a586aceabd09d93ba74c399
atv2016 <alefveld@outlook.com>
2024-03-26 13:12:12 +0000

    Update easy.py

--- easy.py
+++ easy.py
@@ -20,13 +20,13 @@ reader = easyocr.Reader(['en'],gpu=True)
 # Change parameters here. The larger the canvas size the more GPU memory will be used.
 
 if "CANVAS_SIZE" in os.environ:
-		CANVAS_SIZE=int(os.environ["CANVAS_SIZE"])
+	CANVAS_SIZE=int(os.environ["CANVAS_SIZE"])
 else:
-   CANVAS_SIZE=""
+	CANVAS_SIZE=""
 
 if CANVAS_SIZE:
-  print(f"Using custom canvas size of { CANVAS_SIZE }")
-  result = reader.readtext(sys.argv[1], canvas_size=CANVAS_SIZE,detail=0)
+	print(f"Using custom canvas size of { CANVAS_SIZE }")
+	result = reader.readtext(sys.argv[1], canvas_size=CANVAS_SIZE,detail=0)
 else:
 	print("Using default canvas size of 2560")
 	result = reader.readtext(sys.argv[1],detail=0)
